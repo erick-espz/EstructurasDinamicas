f8a2827dbf7b9f5fd6405a21a3842811a875a35d
EArana <eaarana@uamv.edu.ni>
2025-10-15 10:20:09 -0600

    Formulario Diseño

---
+++ EstructuraDinamica/FrmPilaEA.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace EstructuraDinamica
+{
+    partial class FrmPilaEA
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblEdad = new System.Windows.Forms.Label();
+            this.tbEdad = new System.Windows.Forms.TextBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.lblEdades = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblEdad
+            // 
+            this.lblEdad.AutoSize = true;
+            this.lblEdad.Location = new System.Drawing.Point(27, 47);
+            this.lblEdad.Name = "lblEdad";
+            this.lblEdad.Size = new System.Drawing.Size(51, 20);
+            this.lblEdad.TabIndex = 0;
+            this.lblEdad.Text = "Edad:";
+            // 
+            // tbEdad
+            // 
+            this.tbEdad.Location = new System.Drawing.Point(84, 47);
+            this.tbEdad.Name = "tbEdad";
+            this.tbEdad.Size = new System.Drawing.Size(147, 26);
+            this.tbEdad.TabIndex = 1;
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(237, 39);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(90, 43);
+            this.btnAgregar.TabIndex = 2;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            // 
+            // lblEdades
+            // 
+            this.lblEdades.AutoSize = true;
+            this.lblEdades.Location = new System.Drawing.Point(27, 95);
+            this.lblEdades.Name = "lblEdades";
+            this.lblEdades.Size = new System.Drawing.Size(64, 20);
+            this.lblEdades.TabIndex = 3;
+            this.lblEdades.Text = "Edades";
+            // 
+            // FrmPilaEA
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(486, 450);
+            this.Controls.Add(this.lblEdades);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.tbEdad);
+            this.Controls.Add(this.lblEdad);
+            this.Name = "FrmPilaEA";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblEdad;
+        private System.Windows.Forms.TextBox tbEdad;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Label lblEdades;
+    }
+}
+
--- EstructuraDinamica/FrmPilaEA.cs
+++ EstructuraDinamica/FrmPilaEA.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace EstructuraDinamica
 {
-    public partial class Form1 : Form
+    public partial class FrmPilaEA : Form
     {
-        public Form1()
+        public FrmPilaEA()
         {
             InitializeComponent();
         }
